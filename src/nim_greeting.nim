45a4844dca90e7877d870a62eda02e070fc5dce9
Justin Hurst <jryanhurst@gmail.com>
2018-12-17 11:01:07 -0700

    Greet one name

---
+++ src/nim_greeting.nim
@@ -0,0 +1,8 @@
+# This is just an example to get you started. A typical library package
+# exports the main API in this file. Note that you cannot rename this file
+# but you can remove it if you wish.
+
+import strutils
+
+proc greet*(name: string): string =
+  join(["Hello, ", name, "."])
